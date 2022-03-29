2a15aeb0b43b2463ca4d43bd9f48fb3d0ea20102
Zubair Ahmed <zubair.rana@gmail.com>
2022-03-29 17:45:22 +0000

    Added Resource Group for network

--- network.tf
+++ network.tf
@@ -2,5 +2,10 @@ module "network" {
   source  = "app.terraform.io/Best-practises-demo/network/azurerm"
   version = "3.5.0"
   # insert required variables here
-  resource_group_name = "zee-module-testing"
+  resource_group_name = azurerm_resource_group.zee-rg.name
+}
+
+resource "azurerm_resource_group" "zee-rg" {
+  name     = "zee-module-testing"
+  location = "southeastasia"
 }
