15614f4d4f771d5702d79ee03e17eee7946539f5
Zubair Ahmed <zubair.rana@gmail.com>
2022-03-29 17:40:11 +0000

    Added the first tag

---
+++ network.tf
@@ -0,0 +1,6 @@
+module "network" {
+  source  = "app.terraform.io/Best-practises-demo/network/azurerm"
+  version = "3.5.0"
+  # insert required variables here
+  resource_group_name = "zee-module-testing"
+}
